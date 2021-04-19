9be13ad3982b598d4aa0acab056fa970922d1a3d
dev-ideavat <dev@ideavat.com>
2021-04-19 14:07:03 +0530

    Delivery of UI from Abdur Ramij Raj

--- src/App.js
+++ src/App.js
@@ -13,18 +13,18 @@ import "./index.css";
 
 
 const formReducer = (state, event) => {
-  if(event.reset) {
+  if (event.reset) {
     return {
       ChurchLocation: '',
       Attendance: 0,
       Pastorname: '',
     }
   }
- return {
-     ...state,
-     [event.name]: event.value
-   }
- }
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
 
 function App() {
 
@@ -38,90 +38,94 @@ function App() {
     setTimeout(() => {
       setSubmitting(false);
       setFormData({
-       reset: true
-     })
+        reset: true
+      })
     }, 3000);
   }
 
- const handleChange = event => {
-   setFormData({
-     name: event.target.name,
-     value: event.target.value,
-   });
- }
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
 
-  return(
+  return (
 
 
 
     <div className="wrapper">
-    <header className="wrapper-header">
+      <header className="wrapper-header d-flex">
+        <div className="app-logo-update">
           <img src={logo} className="App-logo" alt="logo" />
-      <h1 style={{textAlign: "center"}}>DEEPER LIFE BIBLE CHURCH</h1> <h1 style={{textAlign: "center"}}>DEEPER LIFE SOUTHWEST-USA REGIONAL REPORT</h1> <h1 style={{textAlign: "center"}}> MONTHLY AND WEEKLY ACTIVITY RECORD</h1>
-      {submitting &&
-             <div>Submtting Form...</div>
-           }
-          </header>
-      <form onSubmit = {handleSubmit}>
-      <fieldset disabled={submitting}>
-      <label>
-      <p>Week</p>
-      <select name="Week" onChange={handleChange} value={formData.Week || ''}>
-           <option value = ""> -- Select week--</option>
-           <option value = "Week One">Week One</option>
-           <option value = "Week Two">Week Two</option>
-           <option value = "Week Three">Week Three</option>
-           <option value = "Week Four">Week Four</option>
-       </select>
-       </label>
+        </div>
+        <div style={{ marginLeft: '5%' }}>
+          <h1 style={{ textAlign: "center" }}>DEEPER LIFE BIBLE CHURCH</h1> <h1 style={{ textAlign: "center" }}>DEEPER LIFE SOUTHWEST-USA REGIONAL REPORT</h1> <h1 style={{ textAlign: "center" }}> MONTHLY AND WEEKLY ACTIVITY RECORD</h1>
+        </div>
+        {submitting &&
+          <div>Submtting Form...</div>
+        }
+      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset disabled={submitting}>
+          <label>
+            <p>Week</p>
+            <select name="Week" onChange={handleChange} value={formData.Week || ''}>
+              <option value=""> -- Select week--</option>
+              <option value="Week One">Week One</option>
+              <option value="Week Two">Week Two</option>
+              <option value="Week Three">Week Three</option>
+              <option value="Week Four">Week Four</option>
+            </select>
+          </label>
 
-       <label>
-       <p>Month</p>
-       <select name="Month" onChange={handleChange} value={formData.Month || ''}>
-            <option value = ""> -- Select Month--</option>
-            <option value = "January">January</option>
-            <option value = "February">February</option>
-            <option value = "March">March</option>
-            <option value = "April">April</option>
-        </select>
-        </label>
-     </fieldset>
-      <fieldset disabled={submitting}>
-         <label>
-           <p>Pastor Name</p>
-           <input name="PastorName" onChange={handleChange}value={formData.PastorName || ''} />
-         </label>
-       </fieldset>
-       <fieldset disabled={submitting}>
-       <label>
-       <p>Church Location</p>
-       <select name="ChurchLocation" onChange={handleChange}value={formData.ChurchLocation || ''}>
-            <option value = ""> -- Select a church location--</option>
-            <option value = "Houston">Houston</option>
-            <option value = "Spring">Spring</option>
-            <option value = "Dallas">Dallas</option>
-            <option value = "Austin">Austin</option>
-        </select>
-        </label>
-        <label>
-        <p>Weekly Service</p>
-        <select name="WeeklyService" onChange={handleChange} value={formData.WeeklyService || ''}>
-             <option value = ""> -- Select a day of worship--</option>
-             <option value = "Sunday Service">Sunday Worship Service</option>
-             <option value = "Bible study">Bible Study</option>
-             <option value = "Revival Hour">Revival Hour</option>
-             <option value = "Workers meeting">Workers Meeting</option>
-         </select>
-         </label>
+          <label>
+            <p>Month</p>
+            <select name="Month" onChange={handleChange} value={formData.Month || ''}>
+              <option value=""> -- Select Month--</option>
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+            </select>
+          </label>
+        </fieldset>
+        <fieldset disabled={submitting}>
+          <label>
+            <p>Pastor Name</p>
+            <input name="PastorName" onChange={handleChange} value={formData.PastorName || ''} />
+          </label>
+        </fieldset>
+        <fieldset disabled={submitting}>
+          <label>
+            <p>Church Location</p>
+            <select name="ChurchLocation" onChange={handleChange} value={formData.ChurchLocation || ''}>
+              <option value=""> -- Select a church location--</option>
+              <option value="Houston">Houston</option>
+              <option value="Spring">Spring</option>
+              <option value="Dallas">Dallas</option>
+              <option value="Austin">Austin</option>
+            </select>
+          </label>
+          <label>
+            <p>Weekly Service</p>
+            <select name="WeeklyService" onChange={handleChange} value={formData.WeeklyService || ''}>
+              <option value=""> -- Select a day of worship--</option>
+              <option value="Sunday Service">Sunday Worship Service</option>
+              <option value="Bible study">Bible Study</option>
+              <option value="Revival Hour">Revival Hour</option>
+              <option value="Workers meeting">Workers Meeting</option>
+            </select>
+          </label>
 
-         <label>
-           <p>Attendance</p><label><p>Male</p></label>
-              <input type="number" name="Attendance" onChange={handleChange} step="1" value={formData.Attendance || ''}/>
-         </label>
+          <label>
+            <p>Attendance</p><label><p>Male</p></label>
+            <input type="number" name="Attendance" onChange={handleChange} step="1" value={formData.Attendance || ''} />
+          </label>
 
         </fieldset>
 
-       <button type="submit" disabled={submitting}>Submit</button>
+        <button type="submit" disabled={submitting}>Submit</button>
       </form>
     </div>
   )
---
+++ src/Main/Main.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ChallengeEncounter from '../ChallengeEncounter/challengeEncounter';
+import Headerbar from '../HeaderBar/HeaderBar';
+import logo from "../logo.jfif";
+import TextAreaBox from '../SingleTextArea/singleTextArea';
+import SpecialEvents from '../SpecialEvents/specialEvents';
+import Title from '../Title/Title';
+import WeeklyMeeting from '../WeeklyMeeting/WeeklyMeeting';
+
+const Header = () => (
+    <header className="wrapper-header d-flex-center mt-5">
+        <div className="app-logo-update">
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div style={{ marginLeft: '5%' }}>
+            <h3 style={{ textAlign: "center" }}>DEEPER LIFE BIBLE CHURCH</h3> <h3 style={{ textAlign: "center" }}>DEEPER LIFE SOUTHWEST-USA REGIONAL REPORT</h3> <h3 style={{ textAlign: "center" }}> MONTHLY AND WEEKLY ACTIVITY RECORD</h3>
+        </div>
+    </header>
+)
+
+export default function Main() {
+
+
+    const SubmitButton = () => {
+        return (
+            <>
+                <button className="submit-button">SUBMIT</button>
+            </>
+        )
+    }
+    return (
+        <>
+            <Header />
+            <Title />
+            <Headerbar label="Weekly Meeting Attendance" />
+            <WeeklyMeeting />
+            <Headerbar label="Evangelistic Outreach To Non Members And Visitors" />
+            <TextAreaBox />
+            <Headerbar label="Visitation To Members" />
+            <TextAreaBox />
+            <Headerbar label="Challenges Encounters" />
+            <ChallengeEncounter />
+            <Headerbar label="Special Events And Programs" />
+            <SpecialEvents />
+            <Headerbar label="Testimony/Information" />
+            <TextAreaBox />
+            <Headerbar label="Other Remarks/Observations" />
+            <TextAreaBox />
+            <SubmitButton />
+        </>
+    )
+}
